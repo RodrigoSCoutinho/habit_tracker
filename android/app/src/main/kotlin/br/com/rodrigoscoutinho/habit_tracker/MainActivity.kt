8c94e458a9382619f6dfdb21a6f4c7a34719f25f
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-01-29 15:29:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/rodrigoscoutinho/habit_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.rodrigoscoutinho.habit_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/pages/home_page.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
