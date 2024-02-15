3b792de989c11894cc7e855089a001e2d1a5b900
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-02-15 14:07:47 -0300

    feat: implementing the isa library to app flutter and create the database inserction

---
+++ lib/database/habit_database.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+
+class HabitatDatabase extends ChangeNotifier {
+  static late Isar isar;
+
+  /*
+  
+  SETUP
+  
+  */
+
+
+  // INITIALIZE - DATABASE 
+
+  // Save first data of app startup (for heatmap)
+
+  // Get first data of app startup (for heatmap)
+}
